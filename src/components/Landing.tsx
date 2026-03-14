1c45c3bfc178ac7d2b158d1bd17eb243a6fae681
devangpatil1204 <107292949+devangpatil1204@users.noreply.github.com>
2026-03-14 21:19:04 +0530

    Update name from RAJESH to DEVANG and surname

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,9 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              RAJESH
+              DEVANG
               <br />
-              <span>CHITYAL</span>
+              <span>PATIL</span>
             </h1>
           </div>
           <div className="landing-info">
